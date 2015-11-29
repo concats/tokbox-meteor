500a116bfa7f8cdccebbbb098ed80aa4115dd9d4
Tom Brückner <tom.brueckner@gmx.net>
2015-11-29 20:25:36 +0100

    Added non-minified versions of the JS files and updated opentok.js to 2.6.9

--- package.js
+++ package.js
@@ -1,15 +1,17 @@
 Package.describe({
   summary: "Integrate real-time communications into your website and mobile apps using the OpenTok WebRTC API.",
   version: "2.3.0",
-  name: "aspin:tokbox",
-  git: "https://github.com/aspin/tokbox-meteor"
+  name: "concats:tokbox",
+  git: "https://github.com/concats/tokbox-meteor"
 });
 
-Npm.depends({"opentok" : "2.3.0"});
+Npm.depends({
+  "opentok": "2.3.0"
+});
 
 Package.onUse(function(api) {
   api.versionsFrom('0.9.0');
   api.addFiles('server/index.js', 'server');
-  api.addFiles(['client/opentok.min.js', 'client/opentok-layout.min.js'], 'client');
+  api.addFiles(['client/opentok.js', 'client/opentok-layout.js'], 'client');
   api.export(['OpenTok', 'OpenTokClient'], 'server');
 });
